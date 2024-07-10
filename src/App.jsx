ecb3232b7da5318f950970fdc90a4d01d402dccd
aflores852023 <aflores85@gmail.com>
2024-07-10 17:31:26 -0300

    modificacion final

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,33 @@
-import React, { useState } from 'react'
-import ChatWindow from './components/ChatWindow'
-import MessageInput from './components/MessageInput'
+import React from 'react'
 import './components/App.css'
-
-const DATA_MOCK = [
-  { author: 'pepe', content: 'Hola?', fecha: 'ayer a 15:34', estado: 'visto', id: 1 },
-  { author: 'yo', content: 'Sos real OMG', fecha: 'ayer a 15:35', estado: 'visto', id: 2 },
-  { author: 'pepe', content: 'Obviamente, acaso lo dudaste?', fecha: 'ayer a 15:36', estado: 'visto', id: 3 },
-  { author: 'yo', content: 'Jamas.', fecha: 'ayer a 15:37', estado: 'entregado', id: 4 },
-];
+import VentanaChat from './components/VentanaChat.jsx'
 
 function App() {
-  const arrayMensajes = useState(DATA_MOCK)
-  const mensaje = arrayMensajes[0]
-  const setearMensaje = arrayMensajes[1]
-
-  const agregarMensaje = (nuevoMensaje) => {
-    setearMensaje([mensaje.push(nuevoMensaje)])
-
-    setTiempo(() => {
-      const respuestaMensaje = {
-        author: 'pepe',
-        content: 'Esta es una respuesta automática al chat.',
-        fecha: 'ahora',
-        estado: 'visto',
-        id: mensaje.length + 2,
-      }
-      setearMensaje((prevMensaje) => [mensaje.push(prevMensaje), respuestaMensaje])
-    }, 1000)  
-  }
-
+  
+  const mensajes = [
+    { author: 'pepe', content: 'Hola?', fecha: 'ayer a 15:34', estado: 'visto', id: 1 },
+    { author: 'yo', content: 'Sos real OMG', fecha: 'ayer a 15:35', estado: 'visto', id: 2 },
+    { author: 'pepe', content: 'Obviamente, acaso lo dudaste?', fecha: 'ayer a 15:36', estado: 'visto', id: 3 },
+    { author: 'yo', content: 'Jamas.', fecha: 'ayer a 15:37', estado: 'entregado', id: 4 },
+    ]
+  
+ 
   return (
+
     <div className="App">
-      <ChatWindow mensaje={mensaje} />
-      <MessageInput onSendMessage={agregarMensaje} />
+
+   <div className="chat-window">
+      <div className="message-container">
+        <VentanaChat mensaje={mensajes}/>
+     </div>     
     </div>
+        <div className="message-input">
+          <input type="text" placeholder="Escribe tu mensaje" />  
+          <button>Enviar</button>
+    </div>
+    </div>
+
   )
 }
-
+    
 export default App
