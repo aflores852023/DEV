1dbd2c1a1f8157984eea3e3a1e513619f79d9787
aflores852023 <aflores85@gmail.com>
2024-07-14 18:50:36 -0300

    se agrega foto de pepe

--- src/components/VentanaChat.jsx
+++ src/components/VentanaChat.jsx
@@ -4,6 +4,7 @@ const VentanaChat = ({ mensaje }) => {
   return (
     <div className="chat-window"> 
       <div className="message-container"> 
+      <img src = "src\img\pepe.jpeg"  className='profile-image'/>
         {mensaje.map((msg) => (
           <div key={msg.id} className={`message ${msg.author === 'yo' ? 'my-message' : 'other-message'}`}>  
             <p><strong>{msg.author}</strong>: {msg.content}</p>
