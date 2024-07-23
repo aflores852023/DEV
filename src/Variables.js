5b209c1000038e9d44b641ab151d99cfc3fab233
aflores852023 <aflores85@gmail.com>
2024-07-22 23:45:33 -0300

    primero instalacion react router

---
+++ src/Variables.js
@@ -0,0 +1,18 @@
+/**
+ * Resta dos números.
+ *
+ * @param {number} a - El minuend.
+ * @param {number} b - El subtraendo.
+ * @return {number} La diferencia de los dos números.
+ */
+
+import React from 'react'
+const sumar = (a, b) => {
+    return a + b
+}
+
+const restar = (a, b) => {
+    return a - b
+}   
+export {restar}
+export default sumar
